d73ef4e607c6caf8fd5c0f8ef2e6c524dc90ce06
Alex <alex.d.bondarev@gmail.com>
2019-03-02 20:23:28 +0200

    switch to 'mydatabase' part 2

---
+++ mydatabase/test/integration/default/default_test.rb
@@ -0,0 +1,12 @@
+# # encoding: utf-8
+
+# Inspec test for recipe mydatabase::default
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+
+# This is an example test, replace it with your own test.
+describe port(80), :skip do
+  it { should_not be_listening }
+end
